5db61becd608fb075efcd9373cc6955725d899ee
hinataOhyama <shionote.wp@gmail.com>
2024-06-17 00:32:30 +0000

    add eslint

---
+++ .eslint.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  files: ['**/*.ts', '**/*.tsx'],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+  ],
+  rules: {
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+);
